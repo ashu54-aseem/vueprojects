8d5397920ea6655e0e19e188e5ab3a9530218430
Aseem Saxena <72209771+ashu54-aseem@users.noreply.github.com>
2021-01-27 18:27:01 +0530

    Update to-do-list.vue

--- src/pages/to-do-list.vue
+++ src/pages/to-do-list.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="m-auto">
-    <h1 class="text-3xl text-center my-4">Dc Heros {{ herosCount }}</h1>
+    <h1 class="text-3xl text-center my-4">To-Do-List {{ itemsCount }}</h1>
     <ul class="border rounded p-2">
       <li
         class="flex justify-between"
-        v-for="(hero, index) in dcHeros"
-        :key="hero.name"
+        v-for="(item, index) in to-do-list"
+        :key="item.name"
       >
         <div>
-          {{ hero.name }}
+          {{ item.name }}
         </div>
         <button v-on:click="remove(index)">x</button>
       </li>
     </ul>
-    <form class="mt-10" @submit.prevent="addHero">
+    <form class="mt-10" @submit.prevent="addTtem">
       <input
         class="border rounded px-2"
-        v-model="newHero"
-        placeholder="Type Hero Name Here"
+        v-model="newItem"
+        placeholder="Type Item Name Here"
         ref="newHeroRef"
       />
       <button
         class="border rounded bg-gradient-to-r from-red-700 to-pink-500 text-white ml-2 px-2"
         type="submit"
       >
-        Add Hero
+        Add Item
       </button>
     </form>
   </div>
@@ -34,36 +34,36 @@
 import { computed, onMounted, ref } from "vue";
 export default {
   setup() {
-    const newHeroRef = ref("");
-    const newHero = ref("");
-    const dcHeros = ref([
-      { name: "SuperGirl" },
-      { name: "Flash" },
-      { name: "Batman" },
-      { name: "Arrow" },
-      { name: "SuperMan" },
+    const newItemRef = ref("");
+    const newItem = ref("");
+    const to-do-list = ref([
+      { name: "Breakfast" },
+      { name: "Excersise" },
+      { name: "Lunch" },
+      { name: "Study" },
+      { name: "Dinner" },
     ]);
 
     onMounted(() => {
-      newHeroRef.value.focus();
+      newItemRef.value.focus();
     });
 
-    const herosCount = computed({
-      get: () => dcHeros.value.length,
+    const itemsCount = computed({
+      get: () => to-do-list.value.length,
     });
 
     function remove(index) {
-      dcHeros.value = dcHeros.value.filter((hero, i) => i != index);
+      to-do-list.value = to-do-list.value.filter((item, i) => i != index);
     }
 
-    function addHero() {
-      if (newHero.value !== "") {
-        dcHeros.value.unshift({ name: newHero.value });
-        newHero.value = "";
+    function addItem() {
+      if (newItem.value !== "") {
+        to-do-list.value.unshift({ name: newItem.value });
+        newItem.value = "";
       }
     }
 
-    return { dcHeros, newHero, remove, addHero, newHeroRef, herosCount };
+    return { to-do-list, newItem, remove, addItem, newItemRef, itmesCount };
   },
 };
 </script>
